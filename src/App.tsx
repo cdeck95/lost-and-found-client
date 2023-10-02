cff2a914ca9ae5c52937281d18d6572f44db26bd
Chris Deck <deckchris95@gmail.com>
2023-10-02 15:04:26 -0400

    should be good to go

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import EnterLostDisc from './components/EnterLostDisc';
 import Inventory from './components/Inventory';
+import './styles/App.css';
+import { Button, ButtonGroup, Typography } from '@mui/material'; // Import Button and ButtonGroup from MUI
+
 
 // Define a Disc interface
 export interface Disc {
+  id?: number;
   course: string;
   name: string;
   disc: string;
@@ -14,18 +18,48 @@ export interface Disc {
   dateTexted?: string | null;
   dateClaimed?: string | null;
   status: string;
+  comments?: string | null;
 }
 
+export const API_BASE_URL = 'http://localhost:3001'; // Update the port if needed
+
+
 function App() {
+  const [activeTab, setActiveTab] = useState('enterLostDisc'); // Default active tab
+
+  const switchTab = (tabName: string) => {
+    setActiveTab(tabName);
+  };
+
+ 
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/enter-lost-disc" Component={EnterLostDisc} />
-          <Route path="/inventory" Component={Inventory} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <Typography sx={{ color: 'white', fontSize: "3.5rem"}}>Tranquility Trails Lost and Found</Typography>
+        <nav>
+          <ButtonGroup variant="contained" color="primary">
+            <Button
+              onClick={() => switchTab('enterLostDisc')}
+              color={activeTab === 'enterLostDisc' ? "primary" : "inherit"}
+              className={activeTab === 'enterLostDisc' ? 'active' : ''}
+            >
+              Enter Lost Disc
+            </Button>
+            <Button
+              onClick={() => switchTab('inventory')}
+              color={activeTab === 'inventory' ? "primary" : "inherit"}
+              className={activeTab === 'inventory' ? 'active' : ''}
+            >
+              Inventory
+            </Button>
+          </ButtonGroup>
+        </nav>
+      </header>
+      <main>
+        {activeTab === 'enterLostDisc' && <EnterLostDisc />}
+        {activeTab === 'inventory' && <Inventory />}
+      </main>
+    </div>
   );
 }
 
--- src/components/Inventory.tsx
+++ src/components/Inventory.tsx
@@ -1,49 +1,52 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Disc } from '../App';
+import { API_BASE_URL, Disc } from '../App';
 import '../styles/Inventory.css'; // Import the CSS file
 import { DateTime } from 'luxon';
 
 function Inventory() {
     const [inventory, setInventory] = useState<Disc[]>([]); // Provide the type 'Disc[]'
     const [searchQuery, setSearchQuery] = useState('');
-    const [filteredInventory, setFilteredInventory] = useState([]);
+    const [filteredInventory, setFilteredInventory] = useState(inventory); // Initialize with inventory data
+
 
   // Assuming your API server is running on localhost:3000
-  const API_BASE_URL = 'http://localhost:3001'; // Update the port if needed
 
   const convertToEST = (utcTimestamp: string) => {
     const dateUTC = DateTime.fromISO(utcTimestamp, { zone: 'utc' });
     const dateEST = dateUTC.setZone('America/New_York');
-    return dateEST.toLocaleString(DateTime.DATETIME_SHORT);
+    
+    // Format the date to display only the date (without time)
+    return dateEST.toFormat('yyyy-MM-dd');
   };
 
- useEffect(() => {
-  axios.get(`${API_BASE_URL}/api/inventory`)
-    .then((response) => {
-      // Convert UTC timestamps to EST
-      const convertedInventory = response.data.map((disc: Disc) => ({
-        ...disc,
-        dateFound: convertToEST(disc.dateFound),
-        dateTexted: disc.dateTexted ? convertToEST(disc.dateTexted) : null,
-        dateClaimed: disc.dateClaimed ? convertToEST(disc.dateClaimed) : null,
-      }));
-      
-      setInventory(convertedInventory);
-
-      // Filter the inventory based on the search query
-      const filtered = convertedInventory.filter((disc: Disc) =>
-        disc.phoneNumber.includes(searchQuery) ||
-        disc.disc.includes(searchQuery) ||
-        disc.name.includes(searchQuery)
-      );
-
-      setFilteredInventory(filtered);
-    })
-    .catch((error) => {
-      console.error('Error fetching inventory:', error);
-    });
-}, [searchQuery]); // Trigger the effect when the searchQuery changes
+  useEffect(() => {
+    axios.get(`${API_BASE_URL}/api/inventory`)
+      .then((response) => {
+        // Convert UTC timestamps to EST
+        const convertedInventory = response.data.map((disc: Disc) => ({
+          ...disc,
+          dateFound: convertToEST(disc.dateFound),
+          dateTexted: disc.dateTexted ? convertToEST(disc.dateTexted) : null,
+          dateClaimed: disc.dateClaimed ? convertToEST(disc.dateClaimed) : null,
+        }));
+  
+        setInventory(convertedInventory);
+  
+        // Filter the inventory based on the search query
+        const filtered = convertedInventory.filter((disc: Disc) =>
+          disc.phoneNumber.includes(searchQuery) ||
+          disc.disc.includes(searchQuery) ||
+          disc.name.includes(searchQuery) ||
+          disc.comments?.includes(searchQuery)
+        );
+  
+        setFilteredInventory(filtered);
+      })
+      .catch((error) => {
+        console.error('Error fetching inventory:', error);
+      });
+  }, [searchQuery]);
 
   const markAsClaimed = (discId: string) => {
     axios.put(`${API_BASE_URL}/api/mark-claimed/${discId}`) // Mark a disc as claimed using the API
@@ -70,24 +73,28 @@ function Inventory() {
       <table className="inventory-table"> {/* Apply className */}
         <thead>
           <tr>
+            <th className="table-header">ID</th> {/* Apply className */}
             <th className="table-header">Course</th> {/* Apply className */}
             <th className="table-header">Name</th> {/* Apply className */}
             <th className="table-header">Disc</th> {/* Apply className */}
             <th className="table-header">Phone Number</th> {/* Apply className */}
             <th className="table-header">Bin</th> {/* Apply className */}
             <th className="table-header">Date Found</th> {/* Apply className */}
+            <th className="table-header">Comments</th> {/* Apply className */}
             <th className="table-header">Action</th> {/* Apply className */}
           </tr>
         </thead>
         <tbody>
             {filteredInventory.map((disc: Disc) => (
-            <tr key={disc.disc}>
+            <tr key={disc.id}>
+              <td className="table-cell">{disc.id}</td> {/* Apply className */}
               <td className="table-cell">{disc.course}</td> {/* Apply className */}
               <td className="table-cell">{disc.name}</td> {/* Apply className */}
               <td className="table-cell">{disc.disc}</td> {/* Apply className */}
               <td className="table-cell">{disc.phoneNumber}</td> {/* Apply className */}
               <td className="table-cell">{disc.bin}</td> {/* Apply className */}
               <td className="table-cell">{disc.dateFound}</td> {/* Apply className */}
+              <td className="table-cell">{disc.comments}</td> {/* Apply className */}
               <td className="table-cell">
                 <button onClick={() => markAsClaimed(disc.disc)}>Mark as Claimed</button>
               </td>
